3d4d2ae1bccf2217e985db8636e468b827163950
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2025-06-12 17:23:06 -0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import App from './App.js'
+import { Provider } from 'react-redux';  //allows us to connect our redux store to our react app
+import { store } from './state/store';
+
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <StrictMode>
+      <Provider store={store}> /*our store can now be accessible by any component within our App */
+        <App />
+      </Provider>
+    </StrictMode>
+  );
+} else {
+  console.error('Root element not found!');
+}
+
